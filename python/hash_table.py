623ec2a7fae82f446ae12c1263d71c8fb4574e3b
Vishwajeet Prasad <84278680+vishwajeet7381@users.noreply.github.com>
2025-11-29 21:03:17 +0530

    feat: implement initial HashTable class structure

---
+++ python/hash_table.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+
+class HashTable:
+    def __init__(self, *ele: Any):
+        pass
+
+    def insert(self, *ele: Any):
+        pass
+
+    def remove(self, *ele: Any):
+        pass
+
+    def search(self, ele: Any) -> bool:
+        pass
+
+
+def main():
+    hash_table = HashTable()
+
+
+if __name__ == "__main__":
+    main()
